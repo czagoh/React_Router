d523c96ee787dcbe9ee6826d9ccee59c4060a1b1
czagoh <dorian.pennegat@hotmail.fr>
2026-06-15 11:54:21 +0200

    Next step

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+
+// page components
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Article from "./pages/Article";
+// router creation
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+    path: "/articles/:id",
+    element: <Article />,
+  },
+    ],
+  },
+]);
+
+// rendering
+
+const rootElement = document.getElementById("root");
+
+if (rootElement != null) {
+  ReactDOM.createRoot(rootElement).render(
+    <RouterProvider router={router} />
+  );
+}
---
+++ src/pages/Article.tsx
@@ -0,0 +1,5 @@
+function Article() {
+  return <h1>Hello from Article</h1>;
+}
+
+export default Article;
